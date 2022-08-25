f423f398ba1b14cf859dcd12a84d9cfe9d145a7e
EC2 Default User <ec2-user@ip-172-31-8-34.us-west-2.compute.internal>
2022-08-25 06:52:27 +0000

    [add]4-7aboutページ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,15 @@
 class HomesController < ApplicationController
   def top
   end
+
+
+
+  def about
+  end
+
+
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# root to: "homes#about"
+# get 'about' => "devise/sessions#new"
+
+# resource :about
+get 'about', to: 'homes#about'
+
+
 end
