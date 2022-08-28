2a0309528af746c48ad693f914ebdedb2c81df02
EC2 Default User <ec2-user@ip-172-31-8-34.us-west-2.compute.internal>
2022-08-28 04:44:40 +0000

    [add]4-14usersファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: "homes#top"
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
   get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # delete 'homes/:id' => 'homes#destroy', as: 'destroy_home'
 end
