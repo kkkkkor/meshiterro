690c1691ed96abfdb8efed9fd229ca85374bdb6e
EC2 Default User <ec2-user@ip-172-31-8-34.us-west-2.compute.internal>
2022-08-28 04:26:37 +0000

    [add]4-13投稿削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
   get "/homes/about" => "homes#about", as: "about"
   # delete 'homes/:id' => 'homes#destroy', as: 'destroy_home'
